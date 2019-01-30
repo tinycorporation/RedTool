55b9d4c1dc6ac12352e06c26e9cf24730818a219
rond <r.kulleseid@gmail.com>
2019-01-30 14:11:43 +0100

    Small Update - Added more things to work on.

---
+++ RedTool/frmSelector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedTool
+{
+    public partial class frmSelector : Form
+    {
+        static bool Dev = false;
+        static bool Stable = true;
+
+        public frmSelector()
+        {
+            InitializeComponent();
+
+            if (Settings.Devstatus == 1)
+            {
+                rbDev.Visible = true;
+            }
+            else
+            {
+                rbDev.Visible = false;
+            }
+        }
+
+        private void rbDev_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
