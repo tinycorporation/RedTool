a1c26649a28f5bb09c4b7ac7dede87191e0c0e08
Marcus <isdahl@haugnett.no>
2019-03-26 10:30:05 +0100

    Changed website.

---
+++ Webfiles/js/cookies.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#ffffff"
+        },
+        "button": {
+          "background": "#4b0082"
+        }
+      },
+      "theme": "classic"
+    })});
